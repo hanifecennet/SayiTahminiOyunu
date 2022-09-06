00ea9b1b2b21592781b4259b788a80b60e414d55
Hanife Cennet Alkan <hanifecennetalkan@gmail.com>
2022-09-06 23:56:48 +0300

    SayiTahminOyunu

---
+++ Odev2_SayiTahminiOyunu/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace Odev2_SayiTahminiOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.Window;
+            this.button1.BackColor = System.Drawing.Color.Turquoise;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Corbel", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(314, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "BAŞLA";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(487, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(82, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "TAMAM";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label2.Location = new System.Drawing.Point(226, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "4 basamaklı sayı giriniz:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(420, 169);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(45, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label3.Location = new System.Drawing.Point(226, 217);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "İpucu:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(420, 215);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(45, 22);
+            this.textBox3.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(38, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(420, 262);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(45, 22);
+            this.textBox4.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label5.Location = new System.Drawing.Point(226, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 20);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Tahmin Sayısı:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(208, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(226, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Rastgele sayı üretmek için tıklayınız:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Odev2_SayiTahminiOyunu.Properties.Resources.oyun;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(746, 431);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Sayı Tahmin Oyunu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Odev2_SayiTahminiOyunu/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev2_SayiTahminiOyunu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        { InitializeComponent(); }
+        int erzurum = 0, bursa_sayisi = 0, bursa = 0, manti = 0, sarma = 0, bursa_birler, bursa_onlar, bursa_yuzler, bursa_binler, erzurum_birler, erzurum_onlar, erzurum_yuzler, erzurum_binler;
+        //bursa tahmin edilen sayı, erzurum random sayı, manti = bulunduğu basamak doğru olan sayı
+        //sarma = sayı doğru ama yanlış basamakta
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int c = 0;
+            while (c < 1)
+            {
+                button2.Enabled = true; //tahmin girilebilmesi icin tamam butonunu etkin hale getiririz
+                button1.Enabled = false; //program bitmeden tekrar yeni bir sayi uretilmemesi icin basla butonunu pasif ederiz
+
+                Random rastgele = new Random();
+                erzurum = rastgele.Next(1000, 9999);
+               // textBox1.Text = erzurum.ToString(); //atanan random sayi bunu ekrana yazdirmiyoruz
+
+                //erzurum yani random sayisini basamaklarina ayiriyoruz
+                erzurum_birler = erzurum - ((erzurum / 10) * 10);
+                erzurum = erzurum / 10;
+
+                erzurum_onlar = erzurum - ((erzurum / 10) * 10);
+                erzurum = erzurum / 10;
+
+                erzurum_yuzler = erzurum - ((erzurum / 10) * 10);
+                erzurum = erzurum / 10;
+
+                erzurum_binler = erzurum - ((erzurum / 10) * 10);
+                erzurum = erzurum / 10;
+
+                if (erzurum_birler != erzurum_onlar && erzurum_birler != erzurum_yuzler && erzurum_birler != erzurum_binler && erzurum_onlar != erzurum_yuzler && erzurum_onlar != erzurum_binler && erzurum_yuzler != erzurum_binler)
+                {
+                    c++; //atanan erzurum(random) sayisinin rakamlarinin birbirinden farklı oldugunu kontrol amacli 
+                         //if yapısını kullaniyoruz
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int b = 0;
+
+            do
+            {
+                //her basamağı tek tek karsiastirmak icin bursa(tahmin)sayisinida basamaklarina ayiriyoruz
+                int bursa = Convert.ToInt32(textBox2.Text); //bursa sayisini textBox 2 ye yazdiririz
+                bursa_birler = bursa - ((bursa / 10) * 10);
+                bursa = bursa / 10;
+
+                bursa_onlar = bursa - ((bursa / 10) * 10);
+                bursa = bursa / 10;
+
+                bursa_yuzler = bursa - ((bursa / 10) * 10);
+                bursa = bursa / 10;
+
+                bursa_binler = bursa - ((bursa / 10) * 10);
+                bursa = bursa / 10;
+
+                bursa_sayisi++;
+                textBox4.Text = bursa_sayisi.ToString();
+                 //tahmin isimli int türündeki değişkene texbox2 ye yazılan değerin aktarılmasını sağlıyor
+
+                if (bursa_birler != bursa_onlar && bursa_birler != bursa_yuzler && bursa_birler != bursa_binler && bursa_onlar != bursa_yuzler && bursa_onlar != bursa_binler && bursa_yuzler != bursa_binler)
+
+                    b++; //atanan girilen bursa(tahmin) sayisinin rakamlarinin birbirinden farklı oldugunu kontrol amacli 
+                         //if yapısını kullaniyoruz
+
+                else
+
+                    MessageBox.Show("Geçersiz sayı girdiniz! Lütfen rakamları farklı bir sayı giriniz.", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                //sayinin rakamlari farkli degilse ekrana uyari verir
+
+                manti = 0;
+                sarma = 0;
+                textBox3.Text = "0 "+manti+"0"+sarma; //sayi gecersizse ipucu olarak 0,0 yazar
+            }
+            while (b < 0 && (bursa_birler != bursa_onlar && bursa_birler != bursa_yuzler && bursa_birler != bursa_binler && bursa_onlar != bursa_yuzler && bursa_onlar != bursa_binler && bursa_yuzler != bursa_binler));
+            {
+                if (erzurum_birler == bursa_birler)
+                {                                                                                   //random sayi ile tahmin edilen sayinin basamaklari tek tek karsilastiriliyor
+                    manti++;
+                }
+                if (erzurum_birler == bursa_onlar || erzurum_birler == bursa_yuzler || erzurum_birler == bursa_binler)
+                {
+                    sarma++;
+                }
+                if (erzurum_onlar == bursa_onlar)
+                {
+                    manti++;
+                }
+                if (erzurum_onlar == bursa_birler || erzurum_onlar == bursa_yuzler || erzurum_onlar == bursa_binler)
+                {
+                    sarma++;
+                }
+                if (erzurum_yuzler == bursa_yuzler)
+                {
+                    manti++;
+                }
+                if (erzurum_yuzler == bursa_birler || erzurum_yuzler == bursa_onlar || erzurum_yuzler == bursa_binler)
+                {
+                    sarma++;
+                }
+                if (erzurum_binler == bursa_binler)
+                {
+                    manti++;
+                }
+                if (erzurum_binler == bursa_birler || erzurum_binler == bursa_onlar || erzurum_binler == bursa_yuzler)
+                {
+                    sarma++;
+                
+                }
+            textBox3.Text = "(+" + manti + ",-" + sarma + ")"; //ipucu degerlerini textBox 3 e yazdirir
+            }
+
+            if (manti == 4 && sarma == 0) //ipucu 4,0 olursa programin sona ermesini ve ekrana tebrikler yazilmasini sagliyor
+            {
+                MessageBox.Show("Tebrikler!\n" + bursa_sayisi.ToString() + ". tahminde sayıyı buldunuz.", "MESAJ", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button1.Enabled = true; //oyunun tekrar baslatılması icin basla butonu aktif edilir
+                button2.Enabled = false; //dogru cevap verildiği icin tamam butonu devredisi birakilir
+            }
+        }
+            private void Form1_Load(object sender, EventArgs e)
+            {
+                button2.Enabled = false; //program basladiginda tamam butonu devredisi birakilir
+            }
+        
+    }
+}
